4be4454bec1fe6a180ac4ae62dc75a1e29a8d46b
Shuvam <shuvamsarkar2000@gmail.com>
2023-08-04 21:58:24 +0530

    frontend changes

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import "../styles/home.css";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Hi,this is Shuvam</p>
+        </div>
+    );
+}
+
+export default Home;
